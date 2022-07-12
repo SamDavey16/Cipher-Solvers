75ad5eb6bcd4c4dae526a6cc921957dcaad9e2d9
Sam Davey <32702240+SamDavey16@users.noreply.github.com>
2022-07-12 23:47:44 +0100

    Split code into two files

---
+++ Encrypt_decrypt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cipher_Solver
+{
+    class Solvers
+    {
+        public void Caeser_Encrypt()
+        {
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            Console.WriteLine("Enter text to be encrypted: ");
+            string txt = Console.ReadLine();
+            char[] characters = txt.ToCharArray();
+            Console.WriteLine("Enter shift: ");
+            int shift = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < characters.Length; i++)
+            {
+                int Index = Array.IndexOf(alpha, characters[i]);
+                if (Index + shift >= alpha.Length)
+                {
+                    int distance = alpha.Length - Index;
+                    int sub = Index + shift - alpha.Length;
+                    characters[i] = alpha[sub];
+                }
+                else
+                {
+                    characters[i] = alpha[Index + shift];
+                }
+            }
+            Console.WriteLine(characters);
+        }
+    }
+}
---
+++ Solver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cipher_Solver;
+
+namespace Running
+{
+    class Go
+    {
+        static void Main(string[] args)
+        {
+            Solvers s = new Solvers();
+            s.Caeser_Encrypt();
+        }
+    }
+}
